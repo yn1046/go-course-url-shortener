5b86192f7585ee2da2afc81c8abc6b2dd48c822d
yn1046 <22590687+yn1046@users.noreply.github.com>
2024-10-29 22:10:45 +0300

    first increment commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	body := fmt.Sprintf("Method: %s\r\n", req.Method)
+
+	reqData, _ := io.ReadAll(req.Body)
+
+	body += fmt.Sprintf("Post body: %s\r\n", reqData)
+
+	res.Write([]byte(body))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, mainPage)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yn1046/go-course-url-shortener
+
+go 1.22.0
