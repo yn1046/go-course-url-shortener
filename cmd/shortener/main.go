834d59da7b6a1af6ed40c4154c77e4d9dcf801ba
yn1046 <22590687+yn1046@users.noreply.github.com>
2024-10-30 00:02:21 +0300

    implemented GET and POST endpoints

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,24 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"crypto/sha1"
 	"io"
 	"net/http"
+	"strings"
 )
 
-func mainPage(res http.ResponseWriter, req *http.Request) {
-	body := fmt.Sprintf("Method: %s\r\n", req.Method)
+type MemStorage struct {
+	storage map[string]string
+}
 
-	reqData, _ := io.ReadAll(req.Body)
+var globalStorage MemStorage = MemStorage{storage: make(map[string]string)}
 
-	body += fmt.Sprintf("Post body: %s\r\n", reqData)
+func mainHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		reqData, _ := io.ReadAll(req.Body)
 
-	res.Write([]byte(body))
+		urlString := string(reqData)
+		h := sha1.New()
+		io.WriteString(h, urlString)
+		hashString := string(h.Sum((nil)))[:8]
+
+		globalStorage.storage[hashString] = urlString
+
+		body := "http://localhost:8080/" + urlString
+		res.WriteHeader(http.StatusCreated)
+		res.Write([]byte(body))
+	} else if req.Method == http.MethodGet {
+		hash := strings.TrimPrefix(req.URL.Path, "/")
+		res.Write([]byte(globalStorage.storage[hash]))
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, mainPage)
+	mux.HandleFunc(`/`, mainHandler)
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
